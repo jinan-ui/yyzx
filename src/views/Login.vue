9a6ea82f7d5c1141ba9c4f4ca2e020b53d7ea0f7
liujinan <1502169287@qq.com>
2026-04-14 16:13:23 +0800

    jidenglu

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,18 @@
 <template>
-    <el-row >
+    <!-- 只在这里加了背景样式，其他完全不动 -->
+    <el-row style="height:100vh; background:url(/image.png) center / cover no-repeat fixed;">
         <el-col :xs="{span:24,offset:0}" :sm="{span:8,offset:8}">
-            <h1>东软颐养中心</h1>
+             <div style="text-align:center; margin-bottom:20px;" class="is-full-screen">
+                <img 
+                    src="/图标.png" 
+                    alt="logo" 
+                    style="width:100px; height:100px; vertical-align:middle; margin-right:10px;">
+                <h1 style="display:inline-block; vertical-align:middle; font-size:26px;">东软颐养中心</h1>
+            </div>
             <el-form :model="formData" ref="formData">
                 <el-input placeholder="请输入用户名" prefix-icon="User" 
-                v-model="formData.username" clearable style="margin-bottom: 10px" maxlength="18" show-word-limit></el-input>
+                v-model="formData.username" clearable style="margin-bottom: 10px" maxlength="18" show-word-limit 
+                :status="formData.username ? 'success' : ''"></el-input>
                 <el-input placeholder="请输入密码" prefix-icon="Key" 
                 v-model="formData.password" show-password style="margin-bottom:10px"></el-input>
             </el-form>
@@ -14,7 +22,7 @@
                 </el-option>
             </el-select>
 
-            <el-button type="success" plain style="width: 100%"  :disabled="!formData.username || !formData.password" @click="login">
+            <el-button type="success"  style="width: 100%"  :disabled="!formData.username || !formData.password" @click="login">
                 登录
             </el-button>
         </el-col>
