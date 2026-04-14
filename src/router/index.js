5c4bff16218fca43c1e8a07bdb6d23358dd0e9bb
liujinan <1502169287@qq.com>
2026-04-14 10:18:32 +0800

    jinanluyou

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import UserLogin from '../views/Login.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'UserLogin',
+    component: UserLogin,
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [],
